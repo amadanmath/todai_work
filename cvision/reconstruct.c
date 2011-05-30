37d5f81aa14acb4e0584c0ca3d77308a1cdd6377
Goran Topic <amadan@mad.scientist.com>
2011-05-31 05:55:20 +0900

    Did something...

--- cvision/reconstruct.c
+++ cvision/reconstruct.c
@@ -5,10 +5,22 @@
 
 #define I 3
 #define J 3
-#define D 7
 
 #define F 500.0
 #define B 0.1
+#define ZMIN 2.0
+#define ZMAX 7.5
+
+#define BR B
+#define BL (-B)
+
+
+#define RED(b, x, y) (b->data[56 + ((x + b->rowlen * (b->height1 - y)) * 3)])
+#define GRN(b, x, y) (b->data[55 + ((x + b->rowlen * (b->height1 - y)) * 3)])
+#define BLU(b, x, y) (b->data[54 + ((x + b->rowlen * (b->height1 - y)) * 3)])
+
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
 
 typedef unsigned char  byte;
 typedef unsigned short word;
@@ -17,6 +29,7 @@ typedef unsigned long  dword;
 typedef struct {
   unsigned int width;
   unsigned int height;
+  unsigned int height1;
   unsigned int rowlen;
   unsigned char *data;
 } BITMAP;
@@ -26,50 +39,35 @@ void die(char *str) {
   exit(1);
 }
 
-inline unsigned long getLong(unsigned char *buf) {
+unsigned long get_long(unsigned char *buf) {
   return (unsigned long)buf[0] + ((unsigned long)buf[1] << 8) + ((unsigned long)buf[2] << 16) + ((unsigned long)buf[3] << 24);
 }
-inline unsigned int getInt(unsigned char *buf) {
+
+unsigned int get_int(unsigned char *buf) {
   return (unsigned int)buf[0] + ((unsigned int)buf[1] << 8);
 }
 
 BITMAP *load_bmp(char *file) {
-  static byte buf[54] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0,
-    40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
+  static byte buf[54];
   FILE *f;
+  size_t dummy;
   unsigned int w, h, r;
-  unsigned long filesize;
   f = fopen(file, "rb");
-  fread(buf, 1, 54, f);
+  dummy = fread(buf, 1, 54, f);
   if (buf[0] != 'B' && buf[1] != 'M') die("Not a bitmap");
-  w = getLong(buf + 18);
-  h = getLong(buf + 22);
+  w = get_long(buf + 18);
+  h = get_long(buf + 22);
   r = w;
   while (r & 3) r++; // rowlen divisible by 4
-  if (getInt(buf + 28) != 24) die("Not a 24-bit BMPs");
-  if (getLong(buf + 30)) die("Compressed");
+  if (get_int(buf + 28) != 24) die("Not a 24-bit BMPs");
+  if (get_long(buf + 30)) die("Compressed");
   BITMAP *b = (BITMAP *)malloc(sizeof(BITMAP));
   b->width = w;
   b->height = h;
+  b->height1 = h - 1;
   b->rowlen = r;
-  b->data = (unsigned char *)malloc(filesize = r * h * 3 + 54);
-  printf("read size %dx%d = %lu\n", b->rowlen, b->height, filesize);
-  fread(b->data + 54, 1, r * h * 3, f);
-
-  buf[ 2] = (unsigned char)(filesize      );
-  buf[ 3] = (unsigned char)(filesize >>  8);
-  buf[ 4] = (unsigned char)(filesize >> 16);
-  buf[ 5] = (unsigned char)(filesize >> 24);
-
-  buf[ 4 + 14] = (unsigned char)(b->width       );
-  buf[ 5 + 14] = (unsigned char)(b->width  >>  8);
-  buf[ 6 + 14] = (unsigned char)(b->width  >> 16);
-  buf[ 7 + 14] = (unsigned char)(b->width  >> 24);
-  buf[ 8 + 14] = (unsigned char)(b->height      );
-  buf[ 9 + 14] = (unsigned char)(b->height >>  8);
-  buf[10 + 14] = (unsigned char)(b->height >> 16);
-  buf[11 + 14] = (unsigned char)(b->height >> 24);
-
+  b->data = (unsigned char *)malloc(r * h * 3 + 54);
+  dummy = fread(b->data + 54, 1, r * h * 3, f);
   memcpy(b->data, buf, 54);
   return b;
 }
@@ -90,7 +88,7 @@ BITMAP *clone_bmp(BITMAP *b) {
   return c;
 }
 
-inline void setPixel(BITMAP *b, unsigned int x, unsigned int y,
+inline void set_pixel(BITMAP *b, unsigned int x, unsigned int y,
     unsigned int rr, unsigned int gg, unsigned int bb) {
   if (x >= b->width || y >= b->height) return;
   int offset = 54 + (x + b->rowlen * (b->height - y - 1)) * 3;
@@ -99,25 +97,10 @@ inline void setPixel(BITMAP *b, unsigned int x, unsigned int y,
   b->data[offset + 2] = bb;
 }
 
-inline unsigned char red(BITMAP *b, unsigned int x, unsigned int y) {
-  return b->data[56 + ((x + b->rowlen * (b->height - y - 1)) * 3)];
-}
-inline unsigned char grn(BITMAP *b, unsigned int x, unsigned int y) {
-  return b->data[55 + ((x + b->rowlen * (b->height - y - 1)) * 3)];
-}
-inline unsigned char blu(BITMAP *b, unsigned int x, unsigned int y) {
-  return b->data[54 + ((x + b->rowlen * (b->height - y - 1)) * 3)];
-}
-
-inline double intensity(BITMAP *b, int x, int y) {
-  if (x < 0 || y < 0 || x >= b->width || y >= b->height) return 0;
-  return red(b, x, y) * 0.2989 + grn(b, x, y) * 0.5870 + blu(b, x, y) * 0.1140;
-}
 
 
 void save_bmp(BITMAP *b, char *filename) {
   FILE *f = fopen(filename, "wb");
-  printf("write size %dx%d, filesize %u\n", b->width, b->height, 54 + b->rowlen * b->height * 3);
   fwrite(b->data, 1, 54 + b->rowlen * b->height * 3, f);
 }
 
@@ -129,18 +112,40 @@ double sad(BITMAP *ref, BITMAP *img, int x0, int x1, int y) {
 }
 
 double ssd(BITMAP *ref, BITMAP *img, int x0, int x1, int y) {
-  double item, result = 0;
-  int i, j;
-  for (j = -J; j <= J; j++) {
-    for (i = -I; i <= I; i++) {
-      //item = intensity(img, x0 + i, y + j) - intensity(ref, x1 + i, y + j);
-      //result += item * item;
-      int r = red(img, x0 + i, y + j) - red(ref, x1 + i, y + 1);
-      int g = grn(img, x0 + i, y + j) - grn(ref, x1 + i, y + 1);
-      int b = blu(img, x0 + i, y + j) - blu(ref, x1 + i, y + 1);
+  double item, result=0;
+  int r, g, b;
+  int i, yy, xx0, xx1;
+
+  // make sure we don't access outside the bounds
+  int tsi = MIN(I, x0);
+  int si = -MIN(tsi, x1);
+  int tei = MIN(I, img->width - x0);
+  int ei = MIN(tei, img->width - x1);
+
+  int sy = MAX(y - J, 0);
+  int ey = MIN(y + J + 1, img->height);
+
+  if (sy < 0) sy = 0;
+  if (ey > img->height) ey = img->height;
+
+  //printf("x0:%d(%d-%d); x1:%d(%d-%d); y:%d(%d-%d) - %d, %d\n",
+      //x0, x0+si, x0+ei, x1, x1+si, x1+ei, y, sy, ey, si, ei);
+
+  // sum the value at each pixel
+  for (i = si; i < ei; i++) {
+    xx0 = x0 + i;
+    xx1 = x1 + i;
+    for (yy = sy; yy < ey; yy++) {
+      r = RED(ref, xx0, yy) - RED(img, xx1, yy);
+      g = GRN(ref, xx0, yy) - GRN(img, xx1, yy);
+      b = BLU(ref, xx0, yy) - BLU(img, xx1, yy);
       result += r * r + g * g + b * b;
     }
   }
+
+  // normalise so that the edges get the same treatment as everywhere
+  // else, despite less pixels being tested
+
   return result;
 }
 
@@ -183,15 +188,6 @@ double minimum(int from, int to, double *values) {
   return -b / (2 * a);
 }
 
-double disparity(BITMAP *ref, BITMAP *img, unsigned int x, unsigned int y, int r) {
-  double values[ref->width];
-  int i;
-  for (i = 0; i <= 2 * D; i++) {
-    values[i] = ssd(ref, img, x + r, x + r + i, y);
-  }
-  return x - minimum(x - D, x + D, values) - r;
-}
-
 #define DEBUG_ROWS_START 0
 #define DEBUG_ROWS_END (c->height)
 
@@ -200,51 +196,45 @@ int main(int argc, char **argv) {
   BITMAP *l = load_bmp("viewL.bmp");
   BITMAP *r = load_bmp("viewR.bmp");
   BITMAP *db = clone_bmp(c);
-  int x, y;
-  double xx, yy, zz;
-  double dm[c->width * c->height];
-  double dl, dr;
-  int item;
-  double avg, sd, mind, maxd;
-  FILE *w;
-  //for (y = 0; y < c->height; y++) {
-  for (y = DEBUG_ROWS_START; y < DEBUG_ROWS_END; y+=2) {
-    printf("row %d\n", y);
-    //for (x = 0; x < c->width; x++) {
-    for (x = D; x < c->width - D; x++) {
-      dl = disparity(c, l, x, y, 0);
-//      dr = disparity(c, r, x, y, I);
-//      if (-dr < dl) dl = -dr;
-      dm[x + c->width * y] = dl;
-      sd += dl * dl;
-      avg += dl;
+  int xc, yc, d;
+  int xh = c->width / 2;
+  int yh = c->height / 2;
+  int sd = floor(B * F / ZMAX);
+  int ed = ceil(B * F / ZMIN);
+  double z, xl, xr;
+  double ml, mr;
+  double m[ed - sd + 1];
+  double md;
+  FILE *w = fopen("points.dat", "w");
+  fprintf(w, "%d\n", c->height * c->width);
+
+  for (xc = 0; xc < c->width; xc++) {
+    printf("col %d\n", xc);
+    for (yc = 0; yc < c->height; yc++) {
+      for (d = sd; d <= ed; d++) {
+        z = B * F / d;
+        xl = xc - BL * F / z;
+        xr = xc - BR * F / z;
+        ml = ssd(c, l, xc, xl, yc);
+        mr = ssd(c, r, xc, xr, yc);
+        m[d - sd] = ml + mr;
+      }
+      md = minimum(sd, ed, m);
+      z = B * F / md;
+      fprintf(w, "%f\t%f\t%f\t%d\t%d\t%d\n",
+          (xc - xh) * z / F,
+          (yh - yc) * z / F,
+          z,
+          RED(c, xc, yc),
+          GRN(c, xc, yc),
+          BLU(c, xc, yc));
+
+      // DEBUG:
+      db->data[56 + (xc + db->rowlen * (db->height - yc - 1)) * 3] = (md - sd) * 256 / (ed - sd);
     }
   }
-  sd = sqrt(sd / (c->width * c->height));
-  avg = avg / (c->width * c->height);
-  mind = avg - 3 * sd;
-  maxd = avg + 3 * sd;
-  printf("%f to %f\n", mind, maxd);
-  w = fopen("points.dat", "w");
-  //for (y = 0; y < c->height; y++) {
-  for (y = DEBUG_ROWS_START; y < DEBUG_ROWS_END; y+=2) {
-    //for (x = 0; x < c->width; x++) {
-    for (x = D; x < c->width - D; x++) {
-      dl = dm[x + c->width * y];
-      item = (int)((dl - mind) * 256 / (maxd - mind));
-      if (item < 0) item = 0;
-      else if (item > 255) item = 255;
-      zz = - B * F / dl;
-      xx = - x * zz / F;
-      yy = - y * zz / F;
-      setPixel(db, x, y, item, item, item);
-      fprintf(w, "%f\t%f\t%f\t%d\t%d\t%d\t%d\t%d\n", xx, yy, zz, red(c, x, y), grn(c, x, y), blu(c, x, y), x, y);
-    }
-  }
-  fclose(w);
-  save_bmp(db, "dist-viewL3.bmp");
+  save_bmp(db, "dist-view.bmp");
   free_bmp(db);
   free_bmp(l);
   free_bmp(c);
 }
-
